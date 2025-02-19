d6782827c3035277be25c453ab98307453456204
zhyar <zhyaraland123@gmail.com>
2025-02-20 00:48:19 +0300

    news backend reviewing is done

--- backend/controllers/News/News.js
+++ backend/controllers/News/News.js
@@ -1,4 +1,5 @@
 const { db } = require('../../config/Firebase/firebase');
+const xss = require('xss');
 
 const getAllNews = async (req, res) => {
     try {
@@ -6,8 +7,7 @@ const getAllNews = async (req, res) => {
         const newsData = news.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
         return res.status(200).json({ newsData });
     } catch (error) {
-        console.error('Error getting news:', error);
-        return res.status(500).json({ message: 'Something went wrong' });
+        return res.status(500).json({ message: '‌هەڵەیەک ڕویدا تکایە هەوڵ بدەوە' });
     }
 };
 
@@ -17,41 +17,41 @@ const getNewestNews = async (req, res) => {
         const newsData = news.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
         return res.status(200).json({ newsData });
     } catch (error) {
-        console.error('Error getting news:', error);
-        return res.status(500).json({ message: 'Something went wrong' });
+        return res.status(500).json({ message: '‌هەڵەیەک ڕویدا تکایە هەوڵ بدەوە' });
     }
 }
 
 const getSpecificNews = async (req, res) => {
     const { newsId } = req.params;
+    const sanNewsId = xss(newsId);
+
     try {
-        const newsDoc = await db.collection('News').doc(newsId).get();
+        const newsDoc = await db.collection('News').doc(sanNewsId).get();
         if (!newsDoc.exists) {
-            return res.status(404).json({ message: 'News not found' });
+            return res.status(404).json({ message: 'هەواڵەکە نەدۆزرایەوە' });
         }
         const newsData = newsDoc.data();
         return res.status(200).json({ newsData });
     } catch (error) {
-        console.error('Error getting news:', error);
-        return res.status(500).json({ message: 'Something went wrong' });
+        return res.status(500).json({ message: '‌هەڵەیەک ڕویدا تکایە هەوڵ بدەوە' });
     }
 };
 
 const incrementViewNews = async (req, res) => {
     const { newsId } = req.params;
-    console.log(newsId);
+    const sanNewsId = xss(newsId);
+
     try {
         const collectionRef = db.collection("News");
-        const docRef = collectionRef.doc(newsId);
+        const docRef = collectionRef.doc(sanNewsId);
         const doc = await docRef.get();
         if (!doc.exists) {
-            return res.status(404).json({ message: 'Document not found' });
+            return res.status(404).json({ message: 'هەواڵەکە نەدۆزرایەوە' });
         }
         await docRef.update({ view: doc.data().view + 1 });
-        return res.status(200).json({ message: 'View count incremented successfully' });
+        return res.status(200);
     } catch (error) {
-        console.error('Error incrementing view count:', error);
-        return res.status(500).json({ message: 'Something went wrong' });
+        return res.status(500).json({ message: '‌هەڵەیەک ڕویدا تکایە هەوڵ بدەوە' });
     }
 };
 
